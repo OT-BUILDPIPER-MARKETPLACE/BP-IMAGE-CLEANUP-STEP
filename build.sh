47883b67263fc45c64c46ad4101c6131370aee41
Sandeep <sandeep@opstree.com>
2022-10-18 15:13:30 +0530

    Added a validation of image tag

--- build.sh
+++ build.sh
@@ -20,16 +20,16 @@ function removeImageTags() {
     done
 }
 
-if [ -z "$IMAGE_NAME" ]
+if [ -z "$IMAGE_NAME" ] || [ -z "$IMAGE_TAG" ]
 then
-    logInfoMessage "Image name is not provided in env variable $IMAGE_NAME checking it in BP data"
+    logInfoMessage "Image name/tag is not provided in env variable $IMAGE_NAME checking it in BP data"
     IMAGE_NAME=`getComponentName`
     IMAGE_TAG=`getRepositoryTag`
 fi
 
-if [ -z "$IMAGE_NAME" ]
+if [ -z "$IMAGE_NAME" ] || [ -z "$IMAGE_TAG" ]
 then
-    logErrorMessage "Image name is not available in BP data as well please check!!!!!!"
+    logErrorMessage "Image name/tag is not available in BP data as well please check!!!!!!"
     STATUS=ERROR
 else
     logInfoMessage "I'll remove all prior tagged images of ${IMAGE_NAME}:${IMAGE_TAG}"
