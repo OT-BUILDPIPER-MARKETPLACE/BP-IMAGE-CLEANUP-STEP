0979b82a75e50a5f31c8023b0fc5ebd38a303cae
Sandeep <sandeep@opstree.com>
2022-10-18 14:58:08 +0530

    Updated code to cleanup of older images

--- build.sh
+++ build.sh
@@ -17,5 +17,23 @@ then
 else
     logInfoMessage "I'll remove all prior tagged images of ${IMAGE_NAME}:${IMAGE_TAG}"
     sleep  $SLEEP_DURATION
+    TAGS_LIST=`getImageOlderTags nginx stable-alpine`
+    removeImageTags ${IMAGE_NAME} "${TAGS_LIST}"
 fi
 
+
+function getImageOlderTags() {
+    IMAGE_NAME=$1
+    RECENT_TAG=$2
+    docker images ${IMAGE_NAME} --filter "before=${IMAGE_NAME}:${RECENT_TAG}" --format "{{.Tag}}"
+}
+
+function removeImageTags() {
+    IMAGE_NAME=$1
+    TAGS_LIST="$2"
+
+    for TAG in $TAGS_LIST
+    do 
+        echo "Removing image ${IMAGE_NAME}:${TAG}"
+    done
+}
