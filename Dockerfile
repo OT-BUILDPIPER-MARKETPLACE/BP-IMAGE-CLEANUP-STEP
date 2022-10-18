33cbeace1803fa4071d18357ca42843935874b2f
Sandeep <sandeep@opstree.com>
2022-10-18 14:45:49 +0530

    Updated the image cleanup philosophy

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ RUN apk add docker-cli
 COPY build.sh . 
 RUN chmod +x build.sh
 COPY BP-BASE-SHELL-STEPS/functions.sh . 
-ENV MAX_IMAGES 2 
 ENV SLEEP_DURATION 5s
 ENV ACTIVITY_SUB_TASK_CODE IMAGE_CLEANUP
 ENTRYPOINT [ "./build.sh" ]
--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ if [ -z "$IMAGE_NAME" ]
 then
     logInfoMessage "Image name is not provided in env variable $IMAGE_NAME checking it in BP data"
     IMAGE_NAME=`getComponentName`
+    IMAGE_TAG=`getRepositoryTag`
 fi
 
 if [ -z "$IMAGE_NAME" ]
@@ -14,7 +15,7 @@ then
     logErrorMessage "Image name is not available in BP data as well please check!!!!!!"
     STATUS=ERROR
 else
-    logInfoMessage "I'll retain only ${MAX_IMAGES} images of ${IMAGE_NAME}"
+    logInfoMessage "I'll remove all prior tagged images of ${IMAGE_NAME}:${IMAGE_TAG}"
     sleep  $SLEEP_DURATION
 fi
 
