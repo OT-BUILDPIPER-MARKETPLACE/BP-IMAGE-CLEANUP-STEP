617db74ab109bb2eb81fe4940bd4732c07be240f
Sandeep Rawat <sandeep@opstree.com>
2022-10-20 11:22:47 +0530

    PR for BP step to do image cleanup (#2)
    * Added initial README
    
    * Added BP base shell steps as sub module
    
    * Added basic build.sh
    
    * Added Dockerfile to create dockerized step
    
    * Updated code to get the image name
    
    * Updated build.sh to test image name
    
    * Fixed variable name
    
    * Updated the image cleanup philosophy
    
    * Updated code to cleanup of older images
    
    * Updated code to cleanup of older images
    
    * Added code to delete the image actually
    
    * Updated README on how to use this step
    
    * Added code for BP step
    
    * Added code for warning check
    
    * Added a validation of image tag
    
    * Added debug statement for image name & tag

---
+++ build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+source functions.sh
+
+STATUS="IN_PROGRESS"
+
+function getImageOlderTags() {
+    IMAGE_NAME=$1
+    RECENT_TAG=$2
+    docker images ${IMAGE_NAME} --filter "before=${IMAGE_NAME}:${RECENT_TAG}" --format "{{.Tag}}"
+}
+
+function removeImageTags() {
+    IMAGE_NAME=$1
+    TAGS_LIST="$2"
+
+    for TAG in $TAGS_LIST
+    do 
+        logInfoMessage "Removing image ${IMAGE_NAME}:${TAG}"
+        docker rmi -f ${IMAGE_NAME}:${TAG}
+    done
+}
+
+if [ -z "$IMAGE_NAME" ] || [ -z "$IMAGE_TAG" ]
+then
+    logInfoMessage "Image name/tag is not provided in env variable $IMAGE_NAME checking it in BP data"
+    logInfoMessage "Image Name -> ${IMAGE_NAME}"
+    logInfoMessage "Image Tag -> ${IMAGE_TAG}"
+    IMAGE_NAME=`getComponentName`
+    IMAGE_TAG=`getRepositoryTag`
+fi
+
+if [ -z "$IMAGE_NAME" ] || [ -z "$IMAGE_TAG" ]
+then
+    logErrorMessage "Image name/tag is not available in BP data as well please check!!!!!!"
+    logInfoMessage "Image Name -> ${IMAGE_NAME}"
+    logInfoMessage "Image Tag -> ${IMAGE_TAG}"
+    STATUS=ERROR
+else
+    logInfoMessage "I'll remove all prior tagged images of ${IMAGE_NAME}:${IMAGE_TAG}"
+    sleep  $SLEEP_DURATION
+    TAGS_LIST=`getImageOlderTags nginx stable-alpine`
+    removeImageTags ${IMAGE_NAME} "${TAGS_LIST}"
+    STATUS="success"
+fi
+
+if [ "$STATUS" = "success" ]
+then
+  logInfoMessage "Congratulations clean happenned susccessfully!!!"
+  generateOutput ${ACTIVITY_SUB_TASK_CODE} true "Congratulations clean happenned susccessfully!!!"
+elif [ $VALIDATION_FAILURE_ACTION == "FAILURE" ]
+  then
+    logErrorMessage "Please check logs for failure!!!"
+    generateOutput ${ACTIVITY_SUB_TASK_CODE} false "Please check logs for failure!!!"
+    echo "build unsucessfull"
+    exit 1
+   else
+    logWarningMessage "Please check logs for failure!!!"
+    generateOutput ${ACTIVITY_SUB_TASK_CODE} true "Please check logs for failure!!!"
+fi
