05fd1e06085e695b5b65efdfed264bebad605b76
AniruddhD <aniporwal00@gmail.com>
2026-04-15 17:18:43 +0530

    events added

--- build.sh
+++ build.sh
@@ -9,6 +9,11 @@ source /opt/buildpiper/shell-functions/aws-functions.sh
 IMAGE_NAME=`getComponentName`
 IMAGE_TAG=`getRepositoryTag`
 
+# 1. Initialization Phase
+add_event "IMAGE CLEANUP INITIATED" "InProgress" \
+          "Initializing old docker image cleanup process" \
+          "Target Image: ${IMAGE_NAME:-UNKNOWN}"
+
 function getImageOlderTags() {
     IMAGE_NAME=$1
     RECENT_TAG=$2
@@ -30,24 +35,36 @@ function removeImageTags() {
     done
 }
 
+# 2. Validation Phase
 if [ -z "$IMAGE_NAME" ] || [ -z "$IMAGE_TAG" ]
 then
-    logInfoMessage "Image name/tag is not provided in env variable $IMAGE_NAME checking it in BP data"
+    logInfoMessage "Image name or tag is not provided in environment variables. Checking BP data..."
     logInfoMessage "Image Name -> $IMAGE_NAME"
     logInfoMessage "Image Tag -> $IMAGE_TAG"
 fi
 
+# 3. Execution Phase
 if [ -z "$IMAGE_NAME" ] || [ -z "$IMAGE_TAG" ]
 then
-    logErrorMessage "Image name/tag is not available in BP data please check!!!!!!"
+    logErrorMessage "Image name or tag is missing from BP data. Cannot proceed with cleanup."
     logInfoMessage "Image Name -> $IMAGE_NAME"
     logInfoMessage "Image Tag -> $IMAGE_TAG"
+    
+    add_event "IMAGE CLEANUP COMPLETE" "Failed" \
+              "Missing required Image Name or Tag" \
+              "Action: Cleanup Aborted"
+              
     TASK_STATUS=1
 else
-    logInfoMessage "I'll remove all prior tagged images of ${IMAGE_NAME}:${IMAGE_TAG}"
+    logInfoMessage "Removing all prior tagged images for ${IMAGE_NAME}, retaining current tag: ${IMAGE_TAG}"
     sleep  $SLEEP_DURATION
+    
     TAGS_LIST=`getImageOlderTags $IMAGE_NAME $IMAGE_TAG`
     removeImageTags $IMAGE_NAME "$TAGS_LIST"
+    
+    add_event "IMAGE CLEANUP COMPLETE" "Successful" \
+              "Old docker images purged successfully" \
+              "Retained Tag: ${IMAGE_TAG}"
+              
     TASK_STATUS=0
 fi
-
